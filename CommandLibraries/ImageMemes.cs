af0be41e1653a8c3a9c4d292fb78abb878377c2c
LogicalPhallacy <dragoonmac@gmail.com>
2020-08-29 11:35:49 -0700

    moved commands locations/prep for image memes

---
+++ CommandLibraries/ImageMemes.cs
@@ -0,0 +1,14 @@
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext.Builders;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+
+namespace homiebot 
+{
+    class ImageMemes : BaseCommandModule
+    {
+        
+    }
+}
